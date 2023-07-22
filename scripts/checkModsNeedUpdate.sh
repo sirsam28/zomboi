aa51acd3dc9b245cd2d94b5765135de9985f50e2
opariffazman <ariff.azman@proton.me>
2023-07-22 15:25:58 +0800

    fix: modified correct path

--- scripts/checkModsNeedUpdate.sh
+++ scripts/checkModsNeedUpdate.sh
@@ -3,7 +3,7 @@ screen -S zomboid -X stuff "checkModsNeedUpdate\n"
 sleep 2
 if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated; then
   echo 'true'
-  ~/zomboid-scripts/startModsUpdate.sh >/dev/null 2>&1 &
+  ~/scripts/startModsUpdate.sh >/dev/null 2>&1 &
 else
   echo 'false' # Mods are up to date
 fi
