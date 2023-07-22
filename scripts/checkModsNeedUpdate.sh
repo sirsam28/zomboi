25411c201ba73f036abad153c73808dc9428dde3
opariffazman <ariff.azman@proton.me>
2023-07-22 11:35:31 +0800

    refactor: used shell formatter

--- scripts/checkModsNeedUpdate.sh
+++ scripts/checkModsNeedUpdate.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 screen -S zomboid -X stuff "checkModsNeedUpdate\n"
 sleep 2
-if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated
-then
-echo 'zomboid mods are up to date'
+if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated; then
+  echo 'zomboid mods are up to date'
 else
-echo 'running startModsUpdate'
-~/zomboid-scripts/startModsUpdate.sh
+  echo 'running startModsUpdate'
+  ~/zomboid-scripts/startModsUpdate.sh
 fi
