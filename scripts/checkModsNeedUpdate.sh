811016b73e45c57849a168ee14f3840367e7dfad
opariffazman <ariff.azman@proton.me>
2023-07-22 15:17:32 +0800

    chore: reversed condition for testing

--- scripts/checkModsNeedUpdate.sh
+++ scripts/checkModsNeedUpdate.sh
@@ -2,8 +2,8 @@
 screen -S zomboid -X stuff "checkModsNeedUpdate\n"
 sleep 2
 if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated; then
-  echo 'false' # Mods are up to date
+  echo 'true' # Mods are up to date
 else
-  echo 'true'
+  echo 'false'
   ~/zomboid-scripts/startModsUpdate.sh > /dev/null 2>&1 &
 fi
