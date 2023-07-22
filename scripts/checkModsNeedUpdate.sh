466a500d23d6e934669fd325256cee63fc928f7a
opariffazman <ariff.azman@proton.me>
2023-07-22 15:28:13 +0800

    chore: reverted to actual condition with correct path

--- scripts/checkModsNeedUpdate.sh
+++ scripts/checkModsNeedUpdate.sh
@@ -2,8 +2,8 @@
 screen -S zomboid -X stuff "checkModsNeedUpdate\n"
 sleep 2
 if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated; then
-  echo 'true'
-  ~/scripts/startModsUpdate.sh >/dev/null 2>&1 &
-else
   echo 'false' # Mods are up to date
+else
+  echo 'true'
+  ~/zomboi/scripts/startModsUpdate.sh > /dev/null 2>&1 &
 fi
