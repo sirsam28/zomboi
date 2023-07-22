3cbd6a5aaca53b86db3f6624fc1dee3206fbb67b
opariffazman <ariff.azman@proton.me>
2023-07-22 14:49:18 +0800

    feat: modified to return only true/false & start update in background

--- scripts/checkModsNeedUpdate.sh
+++ scripts/checkModsNeedUpdate.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-screen -S zomboid -X stuff "checkModsNeedUpdate\n"
-sleep 2
-if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated; then
-  echo 'zomboid mods are up to date'
+
+# Function to check if mods need an update and return true if they do, otherwise false
+function checkModsNeedUpdate() {
+  # Execute the checkModsNeedUpdate command in the Zomboid screen session
+  screen -S zomboid -X stuff "checkModsNeedUpdate\n"
+  sleep 2
+
+  # Check the Zomboid server's debug log for the 'updated' message
+  if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated; then
+    echo 'false'  # Mods are up to date
+  else
+    echo 'true'   # Mods need an update
+  fi
+}
+
+# Check if mods need an update
+if checkModsNeedUpdate; then
+  # Run the startModsUpdate.sh script in the background
+  ~/zomboid-scripts/startModsUpdate.sh > /dev/null 2>&1 &
+
+  # Return true, indicating that mods are being updated
+  echo 'true'
 else
-  echo 'running startModsUpdate'
-  ~/zomboid-scripts/startModsUpdate.sh
+  echo 'false' # Mods are up to date
 fi
