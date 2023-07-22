bd0f276eda8437fded44bf7b1ba253a4b0e75f69
opariffazman <ariff.azman@proton.me>
2023-07-22 15:21:21 +0800

    fix: reversed the condition properly

--- scripts/checkModsNeedUpdate.sh
+++ scripts/checkModsNeedUpdate.sh
@@ -2,8 +2,8 @@
 screen -S zomboid -X stuff "checkModsNeedUpdate\n"
 sleep 2
 if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated; then
-  echo 'true' # Mods are up to date
+  echo 'true'
+  ~/zomboid-scripts/startModsUpdate.sh >/dev/null 2>&1 &
 else
-  echo 'false'
-  ~/zomboid-scripts/startModsUpdate.sh > /dev/null 2>&1 &
+  echo 'false' # Mods are up to date
 fi
