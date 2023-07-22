bcdbd235914bcc78eb09a23ae01f961204a28aa1
opariffazman <ariff.azman@proton.me>
2023-07-22 17:20:33 +0800

    feat: reworked with discord webhook

--- scripts/startModsUpdate.sh
+++ scripts/startModsUpdate.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
-echo 'broadcasting message: server will be restarting in 60 seconds'
-screen -S zomboid -X stuff "servermsg \"server will be restarting in 60 seconds\"\n"
-sleep 60
-screen -S zomboid -X stuff "servermsg \"restarting Ssrver\"\n"
-sleep 3
+# Load environment variables from .env file
+source .env
 
-echo 'shutting down server'
+send_discord_message() {
+    local message="$1"
+    local webhook_url="$2"
+    local payload="{
+        \"content\": \"$message\"
+    }"
+    curl -H "Content-Type: application/json" -d "$payload" "$webhook_url"
+}
+
+# Notify users that the server  will be shutting down in 60 seconds
+send_discord_message "Alert! Off to secret mission in 60 seconds" "$NOTICE_WEBHOOK"
+screen -S zomboid -X stuff "servermsg \"Alert! Spiffo need to do stuffs in 60 seconds\"\n"
+sleep 60
+
+# Notify users that the server is shutting down
+send_discord_message "Going dark" "$QUIT_WEBHOOK"
 screen -S zomboid -X stuff "quit\n"
 sleep 15
 
-echo 'updating server'
+# Notify users that the server is updating
+send_discord_message "Doing secret mission" "$UPDATE_WEBHOOK"
 steamcmd +runscript $HOME/update_zomboid.txt
 
-echo "starting zomboid server"
+# Notify users that the server is starting up
+send_discord_message "Calling for extraction" "$START_WEBHOOK"
 screen -dmS zomboid /opt/pzserver/start-server.sh
+sleep 15
+
+# Notify users that the server is up and running
+send_discord_message "Reached the apocalypse" "$READY_WEBHOOK"
