87e4a320cd7a6245fd7a612c006894f0bc2c6717
opariffazman <ariff.azman@proton.me>
2023-07-23 12:22:17 +0800

    refactor: removed unnecessary code

--- server.py
+++ server.py
@@ -1,8 +1,5 @@
 from datetime import datetime
 from discord.ext import tasks, commands
-from file_read_backwards import FileReadBackwards
-from pathlib import Path
-import glob
 import os
 import asyncio
 
@@ -20,33 +17,6 @@ class ServerHandler(commands.Cog):
             self.bot.log.info(f"script path: {self.scriptPath}")
         self.update.start()
         self.webhook = None
-
-    def splitLine(self, line: str):
-        """Split a log line into a timestamp and the remaining message"""
-        timestampStr, message = line.strip()[1:].split("]", 1)
-        timestamp = datetime.strptime(timestampStr, "%d-%m-%y %H:%M:%S.%f")
-        return timestamp, message
-
-    @tasks.loop(minutes=30)
-    async def update(self):
-        """Update the handler
-
-        This will check the latest log file and update our data based on any
-        new entries
-        """
-        files = glob.glob(self.logPath + "/*server.txt")
-        if len(files) > 0:
-            with FileReadBackwards(files[0], encoding="utf-8") as f:
-                newTimestamp = self.lastUpdateTimestamp
-                for line in f:
-                    timestamp, message = self.splitLine(line)
-                    if timestamp > newTimestamp:
-                        newTimestamp = timestamp
-                    if timestamp > self.lastUpdateTimestamp:
-                        await self.handleLog(timestamp, message)
-                    else:
-                        break
-                self.lastUpdateTimestamp = newTimestamp
 
     # Function to run the shell script
     async def runScript(self, scriptPath):
