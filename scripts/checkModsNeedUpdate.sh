12403d06c54055bbd104a884345ff44560bd1a8d
opariffazman <ariff.azman@proton.me>
2023-07-24 11:49:19 +0800

    fix: modified calling startModsUpdate.sh

--- scripts/checkModsNeedUpdate.sh
+++ scripts/checkModsNeedUpdate.sh
@@ -5,5 +5,5 @@ if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated; then
   echo 'false' # Mods are up to date
 else
   echo 'true'
-  ./startModsUpdate.sh > /dev/null 2>&1 &
+  ./startModsUpdate.sh
 fi
