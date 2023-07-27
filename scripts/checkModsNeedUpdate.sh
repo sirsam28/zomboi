73d5bca696fc6412536a2bff9b0def23976cf542
opariffazman <ariff.azman@proton.me>
2023-07-27 08:18:14 +0800

    fix: revert script path

--- scripts/checkModsNeedUpdate.sh
+++ scripts/checkModsNeedUpdate.sh
@@ -5,5 +5,5 @@ if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated; then
   echo 'false' # Mods are up to date
 else
   echo 'true'
-  ~/zomboi/scripts/startModsUpdate.sh
+  ./startModsUpdate.sh
 fi
