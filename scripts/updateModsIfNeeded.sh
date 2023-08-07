e53c6695c249be7c9fe0069734d0c5f00823dba6
opariffazman <ariff.azman@proton.me>
2023-08-07 12:10:35 +0800

    feat: initial commit

---
+++ scripts/updateModsIfNeeded.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+screen -S zomboid -X stuff "checkModsNeedUpdate\n"
+sleep 2
+if tail ~/Zomboid/Logs/*DebugLog-server.txt -n 1 | grep -q updated; then
+  echo 'false' # Mods are up to date
+else
+  echo 'true' # Mods need update
+  ~/zomboi/scripts/startModsUpdate.sh
+fi
